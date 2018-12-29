c0a64b075969c3d1755694a4f906b73f0ad3b7fe
Phongsuphat Sanwong <mebiusgia@gmail.com>
2018-12-29 19:57:00 +0700

    v2

--- Todolist/TodolistVC.swift
+++ Todolist/TodolistVC.swift
@@ -7,11 +7,12 @@
 //
 
 import UIKit
+import CoreData
 
 class TodolistVC: UITableViewController {
     
-    let itemArray = ["Luffy","Zoro","Sanji"]
-
+    var itemArray = ["Luffy","Zoro","Sanji"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,22 +23,47 @@ class TodolistVC: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Todoitemcell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
+        
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(itemArray[indexPath.row])
+        
+        //        print(itemArray[indexPath.row])
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else {
+        } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         tableView.deselectRow(at: indexPath, animated: true)
+        
     }
     
+    @IBAction func addPressed(_ sender: UIBarButtonItem) {
+        
+        var textfield = UITextField()
+        let alert = UIAlertController(title: "Add a new list", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add now", style: .default) { (action) in
+            //What will happen when button was pressed
+            self.itemArray.append(textfield.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextfield) in
+            alertTextfield.placeholder = "Create new list"
+            textfield = alertTextfield
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
     
-
 }
+
+
+
 
