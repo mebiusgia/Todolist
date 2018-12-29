6d5d9312de5872768f0cde267f06504deff996dc
Phongsuphat Sanwong <mebiusgia@gmail.com>
2018-12-29 18:06:49 +0700

    1st Version

---
+++ Todolist/TodolistVC.swift
@@ -0,0 +1,43 @@
+//
+//  TodolistVC.swift
+//  Todolist
+//
+//  Created by Phongsuphat Sanwong on 29/12/2561 BE.
+//  Copyright © 2561 Phongsuphat Sanwong. All rights reserved.
+//
+
+import UIKit
+
+class TodolistVC: UITableViewController {
+    
+    let itemArray = ["Luffy","Zoro","Sanji"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Todoitemcell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+
+}
+
